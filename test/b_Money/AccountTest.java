a962cbd0d88a112c7dccf18566581e04943510e2
Nhunguy <munnguy@gmail.com>
2026-04-28 19:35:28 +0700

    test: identify bugs in Bank and Account using JUnit failure cases

--- test/b_Money/AccountTest.java
+++ test/b_Money/AccountTest.java
@@ -24,21 +24,32 @@ public class AccountTest {
 	
 	@Test
 	public void testAddRemoveTimedPayment() {
-		fail("Write test case here");
+		testAccount.addTimedPayment("rent", 10, 10, new Money(5000, SEK), SweBank, "Alice");
+		assertTrue("Thanh toán định kỳ 'rent' phải tồn tại", testAccount.timedPaymentExists("rent"));
+
+		testAccount.removeTimedPayment("rent");
+		assertFalse("Thanh toán định kỳ 'rent' phải bị xóa", testAccount.timedPaymentExists("rent"));
 	}
 	
 	@Test
 	public void testTimedPayment() throws AccountDoesNotExistException {
-		fail("Write test case here");
+		testAccount.addTimedPayment("sub", 1, 1, new Money(1000, SEK), SweBank, "Alice");
+		testAccount.tick();
+
+		assertEquals("Số dư Hans phải bị trừ sau khi tick", 9999000L, (long)testAccount.getBalance().getAmount());
 	}
 
 	@Test
 	public void testAddWithdraw() {
-		fail("Write test case here");
+		Account freshAccount = new Account("Alice", SEK);
+		freshAccount.deposit(new Money(10000, SEK));
+		freshAccount.withdraw(new Money(5000, SEK));
+
+		assertEquals("Withdraw failed", 5000, (long)freshAccount.getBalance().getAmount());
 	}
 	
 	@Test
 	public void testGetBalance() {
-		fail("Write test case here");
+		assertEquals("Thanh toán không đúng số dư hiện tại", 10000000L, (long)testAccount.getBalance().getAmount());
 	}
 }
--- test/b_Money/BankTest.java
+++ test/b_Money/BankTest.java
@@ -24,41 +24,65 @@ public class BankTest {
 
 	@Test
 	public void testGetName() {
-		fail("Write test case here");
+		assertEquals("SweBank", SweBank.getName());
+		assertEquals("Nordea", Nordea.getName());
 	}
 
 	@Test
 	public void testGetCurrency() {
-		fail("Write test case here");
+		assertEquals(SEK, SweBank.getCurrency());
+		assertEquals(DKK, DanskeBank.getCurrency());
 	}
 
 	@Test
 	public void testOpenAccount() throws AccountExistsException, AccountDoesNotExistException {
-		fail("Write test case here");
+		SweBank.openAccount("Alice");
+		try {
+			SweBank.openAccount("Alice");
+			fail("Nên ném ra lỗi AccountExistsException");
+		} catch (AccountExistsException e) {}
 	}
 
 	@Test
 	public void testDeposit() throws AccountDoesNotExistException {
-		fail("Write test case here");
+		SweBank.deposit("Ulrika", new Money(1000, SEK));
+		assertEquals(Integer.valueOf(1000), SweBank.getBalance("Ulrika"));
 	}
 
 	@Test
 	public void testWithdraw() throws AccountDoesNotExistException {
-		fail("Write test case here");
+		SweBank.deposit("Ulrika", new Money(1000, SEK));
+		SweBank.withdraw("Ulrika", new Money(400, SEK));
+		assertEquals(Integer.valueOf(600), SweBank.getBalance("Ulrika"));
 	}
 	
 	@Test
 	public void testGetBalance() throws AccountDoesNotExistException {
-		fail("Write test case here");
+		assertEquals(Integer.valueOf(0), SweBank.getBalance("Bob"));
 	}
 	
 	@Test
 	public void testTransfer() throws AccountDoesNotExistException {
-		fail("Write test case here");
+		SweBank.deposit("Ulrika", new Money(1000, SEK));
+
+		SweBank.transfer("Ulrika", "Bob", new Money(500, SEK));
+		assertEquals(Integer.valueOf(500), SweBank.getBalance("Ulrika"));
+		assertEquals(Integer.valueOf(500), SweBank.getBalance("Bob"));
+
+		SweBank.transfer("Bob", Nordea, "Bob", new Money(200, SEK));
+		assertEquals(Integer.valueOf(300), SweBank.getBalance("Bob"));
+		assertEquals(Integer.valueOf(200), Nordea.getBalance("Bob"));
 	}
 	
 	@Test
 	public void testTimedPayment() throws AccountDoesNotExistException {
-		fail("Write test case here");
+		SweBank.addTimedPayment("Ulrika", "rent", 1, 0, new Money(100, SEK), SweBank, "Bob");
+
+		SweBank.deposit("Ulrika", new Money(1000, SEK));
+
+		SweBank.tick();
+
+		assertEquals(Integer.valueOf(900), SweBank.getBalance("Ulrika"));
+		assertEquals(Integer.valueOf(100), SweBank.getBalance("Bob"));
 	}
 }
