4b13560e2ab91f52639c3fbe92b09c7125f505f5
Nhunguy <munnguy@gmail.com>
2026-04-28 19:32:56 +0700

    fix: correct coordinate and vector logic bugs in Quadrilateral package

--- src/a_Introductory/Vector2D.java
+++ src/a_Introductory/Vector2D.java
@@ -2,23 +2,26 @@ package a_Introductory;
 
 public class Vector2D {
 	public Integer x, y;
-	
+
 	Vector2D(Integer x, Integer y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	/* Construct Vector2D from two points */
 	Vector2D(Point p1, Point p2) {
 		this.x = p2.x - p1.x;
 		this.y = p2.y - p1.y;
 	}
-	
+
 	public int dotProduct(Vector2D v) {
-		return (x * v.x) + (y * v.x);
+//		return (x * v.x) + (y * v.x);
+		// dot = x1*x2 + y1*y2
+		return (x * v.x) + (y * v.y);
 	}
-	
+
 	public boolean isOrthogonalTo(Vector2D v) {
-		return (dotProduct(v) == -1);
+//		return (dotProduct(v) == -1);
+		return (dotProduct(v) == 0);
 	}
 }
